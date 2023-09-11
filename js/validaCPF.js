7aec338db0a84a1124adb7effd7ae20d375d5440
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-09-11 07:46:41 -0300

    Add: validação dos dígitos do CPF

--- js/validaCPF.js
+++ js/validaCPF.js
@@ -2,7 +2,11 @@ export default function eUmCPF(campo) {
 
     const cpf = campo.value.replace(/\.|-/g, "");
     
-    validaNumerosRepetidos(cpf);
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        console.log("Esse CPF não existe");
+    } else {
+        console.log("Esse CPF existe");
+    }
 
     console.log(validaNumerosRepetidos(cpf));
 
@@ -24,5 +28,45 @@ function validaNumerosRepetidos(cpf) {
     ];
 
     return numerosRepetidos.includes(cpf);
+
+};
+
+function validaPrimeiroDigito(cpf) {
+
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[9];
+
+};
+
+function validaSegundoDigito(cpf) {
+
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[10];
 
 };
