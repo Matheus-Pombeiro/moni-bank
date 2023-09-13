a34863005c6ef7b3936e6d3a23151a3dd3dafe5f
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-09-13 19:30:33 -0300

    Add: persistência de dados com localStorage

--- js/script.js
+++ js/script.js
@@ -2,6 +2,25 @@ import eUmCPF from "./validaCPF.js";
 import eMaiorDeIdade from "./validaIdade.js";
 
 const camposDoFormulario = document.querySelectorAll("[required]");
+const formulario = document.querySelector("[data-formulario]");
+
+formulario.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    const listaRespostas = {
+        "nome": e.target.elements["nome"].value,
+        "email": e.target.elements["email"].value,
+        "rg": e.target.elements["rg"].value,
+        "cpf": e.target.elements["cpf"].value,
+        "aniversario": e.target.elements["aniversario"].value
+    }
+
+    localStorage.setItem("cadastro", JSON.stringify(listaRespostas));
+
+    window.location.href = "./abrir-conta-form-2.html";
+
+});
 
 const tiposDeErros = [
     "valueMissing",
